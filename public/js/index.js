4459c0ee8a94c9b3f8a3cd345a53b3f65114908b
eeong <discography8@gmail.com>
2020-12-23 00:09:25 +0900

    201223 weekly weather done

--- public/js/index.js
+++ public/js/index.js
@@ -11,38 +11,47 @@ var sendData = {
 	lang: "kr"
 }
 
-moment.locale('ko');//moment ko지역 설정
+moment.locale('ko');
+
+var mapMaker, container, options, map, html, iwPosition, customWindow, position, pos, mapCenter;
 
 /*********** Function *************/
-//1. 지도 이미지 생성
-//2. 도시정보를 불러와 openweathermap 에 요청
-//3. 코ㄹ백된 각 도시의 날씨정보를 생성된 지도에 표시
 
-var mapMaker,container, options, map, html, iwPosition, customWindow, position, pos;
-container = document.getElementById('map');
+function mapInit(){
+	container = document.getElementById('map');
+	mapCenter = new kakao.maps.LatLng(35.82, 127.44);
 	options = {
-		center: new kakao.maps.LatLng(35.82, 127.48),
+		center: mapCenter,
 		level: 13
 	};
+
 	map = new kakao.maps.Map(container, options);
 	map.setDraggable(false);
 	map.setZoomable(false);
-	$.get('../json/city.json', onGetCity);
+}
+	
+function onResize(){
+	map.setCenter(mapCenter);
 
+}
+
+$(window).resize(onResize);
+mapInit();
+
+$.get('../json/city.json', onGetCity);
 
 function onGetCity(r){
 		r.cities.forEach(function(v, i){
 			sendData.id = null;
 			sendData.lat = v.lat;
 			sendData.lon = v.lon;
-			$.get(dailyURL, sendData, function(res){onGetDaily(res,v.class)});
-			$("#city").append("<option value="+v.id+">" + v.name + "</option>");
+			$.get(dailyURL, sendData, function(res){onGetDaily(res, v.class)});
+			$("#city").append("<option value='"+v.id+"'>" + v.name + "</option>");
 		});
 	}
 
-function onGetDaily(r,v){
+function onGetDaily(r, cls){
 	icon = "https://openweathermap.org/img/wn/"+ r.weather[0].icon +"@2x.png";
-	cls= v;
 	var html;
 	html = '<div id="c'+r.id+'" class="info-window '+ cls+ ' " onclick="onCustomClick('+r.id+')">';
 	html+='<img src="'+icon+'" style="width:40px;">';
@@ -51,33 +60,69 @@ function onGetDaily(r,v){
 	html+='</div>'
   var position = new kakao.maps.LatLng(r.coord.lat , r.coord.lon)
 	var customWindow = new kakao.maps.CustomOverlay({
-    position : position, 
+ 		position : position, 
 		content : html,
 		clickable: true,
 });
 customWindow.setMap(map);
 }
 
+//현재위치
+navigator.geolocation.getCurrentPosition(onGetLoc, onErrorPos);
+
 function onGetLoc(r){
 	sendData.id = null;
 	sendData.lat = r.coords.latitude; 
 	sendData.lon = r.coords.longitude; 
 	dt = new Date();
+	$.get(dailyURL, sendData, onGetDailyWeather);
+	$.get(weeklyURL, sendData, onGetWeeklyWeather);
+}
+
+function onErrorPos(e){
+	console.log(e);
+}
+
+//새로고침
+$(".bt-position").on('click',function(){
+	location.reload();
+});
+
+//풍향함수
+function windDeg(deg){
+	for(var i = 1; i<17; i++){
+		var iArr = ['북','북북동','북동','동북동','동','동남동','남동','남남동','남','남남서','남서','서남서','서','서북서','북서','북북서']
+		if( deg > (i-1)*22.5 && deg <= i*22.5){
+			return iArr[i-1] + '풍';
+		}
+	}
+}
+
+// 지역 변경 이벤트
+$("#city").change(onGetCityWeather);
+
+function onGetCityWeather(){
+	sendData.id = $(this).val();
+	sendData.lat = null;
+	sendData.lon = null;
+	$('.info-window').removeClass('active');
+	$('#c'+sendData.id).addClass('active');
+	$('.info-window').find('.img-arrow').attr('src', '../img/arrow.png')
+	$('#c'+sendData.id).find('.img-arrow').attr('src', '../img/arrow-a.png')
 	$.get(dailyURL,sendData,onGetDailyWeather);
 	$.get(weeklyURL,sendData,onGetWeeklyWeather);
 }
 
+// 지도 클릭 이벤트
+function onCustomClick(id){
+	$("#city").val(id).trigger('change');
+}
+
+//데일리 
 function onGetDailyWeather(r){
 	var dtDate = moment(r.dt * 1000).format('M월 D일');
 	var dtTime = moment(r.dt * 1000).format('H시 m분');
 	var locTitle = r.name+', '+ r.sys.country 
-	function windDeg(deg){
-		for(var i = 1; i<13; i++){
-			var iArr = ['북북동','북동','북동동','남동동','남동','남남동','남남서','남서','남서서','북서서','북서','북북서']
-			if( deg > (i-1)*30 && deg <= i*30){
-				return iArr[i-1] + '풍';
-		}
-		}}
 	icon = "https://openweathermap.org/img/wn/"+ r.weather[0].icon +"@2x.png";
 	$(".loc-wrapper .title-loc").text(locTitle);
 	$(".loc-wrapper .title-date").text(dtDate);
@@ -95,35 +140,67 @@ function onGetDailyWeather(r){
 	$(".misc-wrap .sunset").text(moment(r.sys.sunset * 1000).format('H시 m분'));
 }
 
-// 지역 변경 이벤트
-$("#city").change(onGetCityWeather);
-
-function onGetCityWeather(){
-	sendData.id = $(this).val();
-	sendData.lat = null;
-	sendData.lon = null;
-	$.get(dailyURL,sendData,onGetDailyWeather);
-	$.get(weeklyURL,sendData,onGetWeeklyWeather);
-	$('.info-window').removeClass('active');
-	$('#c'+$(this).val()).addClass('active');
-	$('.info-window').find('.img-arrow').attr('src', '../img/arrow.png')
-	$('#c'+$(this).val()).find('.img-arrow').attr('src', '../img/arrow-a.png')
-	onGetDaily();
-}
-
-
-// 지도 클릭 이벤트
-function onCustomClick(id){
-	$("#city").val(id).trigger('change');
-}
-
 function onGetWeeklyWeather(r){
+	console.log(r)
+	$(".weekly-weather .slide-wrapper").empty();
+	for(var i in r.list) {
+		v = r.list[i];
+		html = '<div class="slide swiper-slide">';
+		html += '<div>';
+		html += '<div class="icon">';
+		html += '<img src="https://openweathermap.org/img/wn/'+v.weather[0].icon+'@2x.png" alt="아이콘">';
+		html += '</div>';
+		html += '<div class="temp-wrap">';
+		html += '<span class="temp">'+v.main.temp+'</span>℃'; 
+		html += '(체감: <span class="temp-feel">'+v.main.feels_like+'</span>℃)';
+		html += '</div>';
+		html += '<div class="press-wrap">';
+		html += '기압: <span class="pressure">'+v.main.pressure+'</span>mmHg<br>';
+		html += '습도: <span class="humidity">'+v.main.humidity+'</span>%';
+		html += '</div>';
+		html += '<div class="wind-wrap">';
+		html += '바람: <span class="wind">'+windDeg(v.wind.deg)+'</span>';
+		html +=  v.wind.speed+'<span class="wind-speed">m/s</span>';
+		html += '</div>';
+		html += '<div class="time">'+moment(v.dt * 1000).format('M월 D일 H시 기준')+'</div>';
+		html += '</div>';
+		html += '</div>';
+		$(".weekly-weather .slide-wrapper").append(html);
+	}
 
+	//swiper count 함수
+	function getCount() {
+		var wid = $(window).outerWidth();
+		var count = 3;
+		if(wid <= 1199 && wid > 991) count = 2;
+		else if(wid <= 991 && wid > 767) count = 3;
+		else if(wid <= 767 && wid > 575) count = 2;
+		else if(wid <= 575) {count = 1; }
+		return count;
+	}
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: getCount(),
+		slidesPerGroup: getCount(),
+		spaceBetween: 0,
+		freeMode: true,
+		loopFillGroupWithBlank: true,
+		navigation: {
+			nextEl: '.bt-angle.bt-next',
+			prevEl: '.bt-angle.bt-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: $(window).outerWidth() > 575 ? true : false
+		},
+	});
+
+	swiper.on("resize", function() {
+		this.params.slidesPerGroup = getCount();
+		this.params.slidesPerView = getCount();
+	});
 }
 
-function error(err){
 
-}
 
-navigator.geolocation.getCurrentPosition(onGetLoc, error, options);
 
